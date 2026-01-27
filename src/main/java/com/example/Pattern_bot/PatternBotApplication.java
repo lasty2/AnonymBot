0a96617cf7c525ef86156369a0c18265230fedfb
lasty <lpgexonlp@gmail.com>
2026-01-27 22:20:21 +0300

    error resolved

--- src/main/java/com/example/Pattern_bot/PatternBotApplication.java
+++ src/main/java/com/example/Pattern_bot/PatternBotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    gPatternBotApplication {
+public class PatternBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatternBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatternBotApplication.class, args);
+    }
 
 }
